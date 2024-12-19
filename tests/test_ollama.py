c6532d9f88b23dcfaa4bb43e220c0476a21d42e4
Sudeep Pillai <sudeep.pillai@gmail.com>
2024-12-19 09:07:17 -0800

    Add dataset scaffolding for testing purposes (#25)

--- tests/test_ollama.py
+++ tests/test_ollama.py
@@ -1,8 +1,9 @@
 import importlib.util
 import pytest
 from loguru import logger
-from vlmrun.hub.schemas.document.invoice import Invoice
-
+from vlmrun.hub.dataset import VLMRUN_HUB_DATASET
+from pydantic import BaseModel
+from typing import Type
 
 
 @pytest.mark.skipif(
@@ -10,27 +11,28 @@ from vlmrun.hub.schemas.document.invoice import Invoice
 )
 def test_local_ollama():
     from ollama import chat
-    from vlmrun.hub.utils import encode_image, remote_image
+    from vlmrun.hub.utils import encode_image
 
-    image_urls = [
-        "https://mintlify.s3.us-west-1.amazonaws.com/autonomiai/guides/doc-ai/images/sample-invoice.jpg",
-    ]
-    images = [remote_image(url) for url in image_urls]
-    chat_response = chat(
-        model="llama3.2-vision:11b",
-        format=Invoice.model_json_schema(),  # Pass in the schema for the response
-        messages=[
-            {
-                "role": "user",
-                "content": "Extract the invoice in JSON format. If you cannot determine certain details, leave those fields empty.",
-                "images": [
-                    encode_image(img, format="JPEG").split(",")[1] for img in images
-                ],
-            },
-        ],
-        options={
-            "temperature": 0
-        },  # Set temperature to 0 for more deterministic output
-    )
-    response: Invoice = Invoice.model_validate_json(chat_response.message.content)
-    logger.info(response.model_dump_json(indent=2))
+    for sample in VLMRUN_HUB_DATASET.values():
+        response_model: Type[BaseModel] = sample.response_model
+        chat_response = chat(
+            model="llama3.2-vision:11b",
+            format=response_model.model_json_schema(),  # Pass in the schema for the response
+            messages=[
+                {
+                    "role": "user",
+                    "content": sample.prompt,
+                    "images": [
+                        encode_image(img, format="JPEG").split(",")[1]
+                        for img in sample.images
+                    ],
+                },
+            ],
+            options={
+                "temperature": 0
+            },  # Set temperature to 0 for more deterministic output
+        )
+        response: Type[BaseModel] = response_model.model_validate_json(
+            chat_response.message.content
+        )
+        logger.info(response.model_dump_json(indent=2))
--- vlmrun/hub/utils.py
+++ vlmrun/hub/utils.py
@@ -1,19 +1,16 @@
 from io import BytesIO
 from pathlib import Path
-from typing import Literal, Union, Iterator
+from typing import Literal, Union
 
 import requests
 from PIL import Image
 from base64 import b64encode
-from cloudpathlib import CloudPath
-import contextlib
 
 VLMRUN_CACHE_DIR = Path.home() / ".vlmrun" / "cache"
 VLMRUN_HUB_DIR = Path.home() / ".vlmrun" / "hub"
 
 VLMRUN_HUB_DIR.mkdir(parents=True, exist_ok=True)
 VLMRUN_CACHE_DIR.mkdir(parents=True, exist_ok=True)
-
 
 
 def encode_image(
@@ -43,7 +40,6 @@ def encode_image(
     return f"data:image/{image_format.lower()};base64,{img_str}"
 
 
-
 _HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
@@ -58,8 +54,11 @@ _HEADERS = {
     "Cache-Control": "max-age=0",
 }
 
+
 def remote_image(url: str | Path) -> Image.Image:
-    assert isinstance(url, (str, Path)), f"Invalid type for url [url={url}, type={type(url)}]"
+    assert isinstance(
+        url, (str, Path)
+    ), f"Invalid type for url [url={url}, type={type(url)}]"
     if url.startswith("http"):
         bytes = requests.get(url, headers=_HEADERS).content
         return Image.open(BytesIO(bytes)).convert("RGB")
