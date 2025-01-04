452663faef7449d5c1d8c0d72641c63be03aa597
Scott Loftin <scott@autonomi.ai>
2025-01-03 19:23:31 -0500

    Pull in test URLs from the app.vlm examples

---
+++ tests/test_urls.py
@@ -0,0 +1,11 @@
+TEST_URLS = {
+    "document.invoice-extraction": "https://mintlify.s3.us-west-1.amazonaws.com/autonomiai/guides/doc-ai/images/sample-invoice.jpg",
+    "healthcare.medical-insurance-card": "https://storage.cloud.google.com/vlm-1/test-data/document.health-insurance-card/2021-22_cu_gold_ship_card_front_0.webp",
+    "retail.ecommerce-product-caption": "https://storage.googleapis.com/vlm-data-public-prod/hub/examples/retail.ecommerce-product-caption/Electronics%20-%20Kindle.webp",
+    "aerospace.remote-sensing": "https://storage.googleapis.com/vlm-data-public-prod/hub/examples/aerospace.remote-sensing/planet_labs_port.jpg",
+    "media.nba-game-s tate": "https://storage.googleapis.com/vlm-data-public-prod/hub/examples/media.nba-game-state/heats_spurs.png",
+    "media.tv-news": "https://storage.googleapis.com/vlm-data-public-prod/hub/examples/media.tv-news/bbc_news_ukraine_screenshot.jpg",
+    "media.tv-caption": "https://storage.googleapis.com/vlm-data-public-prod/hub/examples/media.tv-caption/tv-caption.jpeg",
+    "document.id-extraction": "https://storage.googleapis.com/vlm-data-public-prod/hub/examples/document.id-extraction/usa_passport.webp"
+}
+ 
